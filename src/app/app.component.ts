15e1d93a81213484bc7d0b6a2b592aea57ebbe77
Anartz Mugika <mugan86@gmail.com>
2017-03-11 18:47:52 +0100

    Load with API and show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { SharedService } from "./shared.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app works!';
+
+  seasons: any;
+  URL: string = "";
+
+  constructor(private _sharedService: SharedService) {
+  }
+
+  ngOnInit() {
+    this.getSeasons();
+  }
+
+  getSeasons() {
+    this._sharedService.findF1SeasonsList()
+      .subscribe(
+      lstresult => {
+
+        this.seasons = lstresult["MRData"]["SeasonTable"]["Seasons"];
+        this.URL = lstresult["MRData"]["url"];
+        console.log("URL: " + this.URL);
+        console.log("SEASONS: " + this.seasons[0]["season"]);
+      },
+      error => {
+        console.log("Error. The findWeather result JSON value is as follows:");
+        console.log(error);
+      }
+      );
+  }
 }
