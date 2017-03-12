3eab886558be298d03217658aa1dcf634b0f7971
Anartz Mugika <mugan86@gmail.com>
2017-03-12 18:59:31 +0100

    Add seasons component

---
+++ src/app/seasons/seasons.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from "../shared.service";
+
+@Component({
+  selector: 'app-seasons',
+  templateUrl: './seasons.component.html',
+  styleUrls: ['./seasons.component.css']
+})
+export class SeasonsComponent implements OnInit {
+
+  seasons: any;
+  URL: string = "";
+
+  constructor(private _sharedService: SharedService) {
+  }
+
+  ngOnInit() {
+    this.getSeasons();
+  }
+
+  getSeasons() {
+    this._sharedService.findF1SeasonsList()
+      .subscribe(
+      lstresult => {
+
+        this.seasons = lstresult["MRData"]["SeasonTable"]["Seasons"];
+        this.URL = lstresult["MRData"]["url"];
+        console.log("URL: " + this.URL);
+        console.log("SEASONS: " + this.seasons[0]["season"]);
+      },
+      error => {
+        console.log("Error. The findWeather result JSON value is as follows:");
+        console.log(error);
+      }
+      );
+  }
+
+}
