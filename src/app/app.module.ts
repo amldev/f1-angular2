ce3f2f116e6810e8fcbd3981c807d206efea069d
Anartz Mugika <mugan86@gmail.com>
2017-03-12 19:01:00 +0100

    Add new modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,31 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
 import { SharedService } from "./shared.service";
 //Import Pipes
 import { WikipediaMobileUrlPipe } from './app.pipe';
 
+//routing
+import { ROUTING } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { SeasonsComponent } from './seasons/seasons.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    WikipediaMobileUrlPipe
+    MenuComponent,
+    WikipediaMobileUrlPipe,
+    DriversComponent,
+    SeasonsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ROUTING
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
