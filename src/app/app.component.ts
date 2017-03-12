bd461503a540182b22f8a0576611ccdec0f0ec58
Anartz Mugika <mugan86@gmail.com>
2017-03-12 19:01:26 +0100

    Add router-outlet to use app.routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,30 +11,5 @@ import { SharedService } from "./shared.service";
 export class AppComponent {
   title = 'app works!';
 
-  seasons: any;
-  URL: string = "";
 
-  constructor(private _sharedService: SharedService) {
-  }
-
-  ngOnInit() {
-    this.getSeasons();
-  }
-
-  getSeasons() {
-    this._sharedService.findF1SeasonsList()
-      .subscribe(
-      lstresult => {
-
-        this.seasons = lstresult["MRData"]["SeasonTable"]["Seasons"];
-        this.URL = lstresult["MRData"]["url"];
-        console.log("URL: " + this.URL);
-        console.log("SEASONS: " + this.seasons[0]["season"]);
-      },
-      error => {
-        console.log("Error. The findWeather result JSON value is as follows:");
-        console.log(error);
-      }
-      );
-  }
 }
