56d565fb1478f0fa270c3a9af5cbbae417df061d
Anartz Mugika <mugan86@gmail.com>
2017-03-13 07:31:53 +0100

    Add circuits component

---
+++ src/app/components/circuits/circuits.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { CircuitsComponent } from './circuits.component';
+
+describe('CircuitsComponent', () => {
+  let component: CircuitsComponent;
+  let fixture: ComponentFixture<CircuitsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CircuitsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CircuitsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
