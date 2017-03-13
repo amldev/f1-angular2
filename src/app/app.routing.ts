b6a8ff38277d8d1b3acc43541390bedec3102415
Anartz Mugika <mugan86@gmail.com>
2017-03-13 07:33:05 +0100

    Add new routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DriversComponent } from './components/drivers/drivers.component';
 import { SeasonsComponent } from './components/seasons/seasons.component';
+import { CircuitsComponent } from './components/circuits/circuits.component';
 
 const MAINMENU_ROUTES: Routes = [
     //full : makes sure the path is absolute path
     { path: '', redirectTo: '/seasons', pathMatch: 'full' },
     { path: 'seasons', component: SeasonsComponent },
-    { path: 'drivers', component: DriversComponent }
+    { path: 'drivers', component: DriversComponent },
+    { path: 'circuits', component: CircuitsComponent }
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(MAINMENU_ROUTES);
