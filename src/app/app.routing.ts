823844d5bb76d2fb5b65f431d207dc213614b5e1
Anartz Mugika <mugan86@gmail.com>
2017-03-13 21:05:05 +0100

    Add new routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,19 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { DriversComponent } from './components/drivers/drivers.component';
+import { DriverDetailsComponent } from './components/drivers/driver.component';
 import { SeasonsComponent } from './components/seasons/seasons.component';
 import { CircuitsComponent } from './components/circuits/circuits.component';
+import { PageNotFoundComponent } from './components/notfound/not-found.component'
 
 const MAINMENU_ROUTES: Routes = [
     //full : makes sure the path is absolute path
     { path: '', redirectTo: '/seasons', pathMatch: 'full' },
     { path: 'seasons', component: SeasonsComponent },
     { path: 'drivers', component: DriversComponent },
-    { path: 'circuits', component: CircuitsComponent }
+    { path: 'driver/:id', component: DriverDetailsComponent },
+    { path: 'circuits', component: CircuitsComponent },
+    { path: '**', component: PageNotFoundComponent } //Show when select route is not register
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(MAINMENU_ROUTES);
