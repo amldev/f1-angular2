d3cfbc971446365e484b99cc84febe4a5fe40aba
Anartz Mugika <mugan86@gmail.com>
2017-03-14 08:48:20 +0100

    Manage select driver info

--- src/app/components/drivers/driver.component.ts
+++ src/app/components/drivers/driver.component.ts
@@ -1,27 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from "./../../shared.service";
+import { ActivatedRoute } from "@angular/router";
+import { Driver } from "./driver.model"
 
 @Component({
-  selector: 'app-drivers',
-  templateUrl: './drivers.component.html',
+  selector: 'app-driver',
+  templateUrl: './driver.component.html',
   styleUrls: ['./drivers.component.css']
 })
 export class DriverDetailsComponent implements OnInit {
+  driverId:string;
+  driver: any;
   isLoading: boolean = true;
-  constructor(private _sharedService: SharedService) {}
-//https://angular-2-training-book.rangle.io/handout/routing/routeparams.html
+  private sub:any;
+
+  //Driver data   this.name = this.driver.givenName + this.driver.familyName;
+  name: string;
+  dateOfBirth: string;
+  permanentNumber: number;
+  nationality: string;
+  url: string;
+
+  constructor(private _sharedService: SharedService, private route: ActivatedRoute) {}
+
   ngOnInit() {
-    this.getLastYearDrivers();
+    this.sub = this.route.params.subscribe(params => {
+       this.driverId = params['id']; // (+) converts string 'id' to a number
+
+       // In a real app: dispatch action to load the details here.
+       this.getLastYearDrivers();
+    });
+    
   }
-  drivers: any;
 
   getLastYearDrivers() {
     this.setIsLoadingProgress();
-    this._sharedService.findSelectDriverWithId("alonso")
+    this._sharedService.findSelectDriverWithId(this.driverId)
       .subscribe(
       lstresult => {
         console.log("Load data!");
-        this.drivers = lstresult["MRData"]["DriverTable"]["Drivers"];
+        this.driver = lstresult["MRData"]["DriverTable"]["Drivers"][0];
+        console.log(this.driver.nationality);
+        this.name = this.driver.givenName + " " + this.driver.familyName;
+        this.nationality = this.driver.nationality;
+        this.url = this.driver.url;
+        this.dateOfBirth = this.driver.dateOfBirth;
+        this.permanentNumber = this.driver.permanentNumber;
+        
         this.isLoadingFinish();
       },
       error => {
