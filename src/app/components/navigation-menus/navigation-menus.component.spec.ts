421bcc6028ee8fcb6b149687cc8cbf18dc80e967
Anartz Mugika <mugan86@gmail.com>
2017-03-16 19:19:45 +0100

    Add navigation menus to use in all views

---
+++ src/app/components/navigation-menus/navigation-menus.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { NavigationMenusComponent } from './navigation-menus.component';
+
+describe('NavigationMenusComponent', () => {
+  let component: NavigationMenusComponent;
+  let fixture: ComponentFixture<NavigationMenusComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavigationMenusComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigationMenusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/navigation-menus/navigation-menus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navigation-menus',
+  templateUrl: './navigation-menus.component.html',
+  styleUrls: ['./navigation-menus.component.css']
+})
+export class NavigationMenusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
