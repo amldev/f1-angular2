4397493abc2129c38233e2b31fadc1ad421666cc
Anartz Mugika <mugan86@gmail.com>
2017-03-17 15:47:25 +0100

    Add Google Maps component

---
+++ src/app/components/googlemaps/google-maps.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'google-maps',
+  templateUrl: './google-maps.component.html',
+  styleUrls: ['./google-maps.component.css']
+})
+
+
+export class AppComponent {
+  lat: number = 23.022505;  
+  lng: number = 72.571362;  
+}
