6241fb465628cf4fc681ba86a408b6cdc9eb08b8
Anartz Mugika <mugan86@gmail.com>
2017-03-18 20:30:18 +0100

    Update Google Maps files

--- src/app/components/googlemaps/google-maps.component.ts
+++ src/app/components/googlemaps/google-maps.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MarkerItem } from './marker';
+import { Marker } from './marker';
 
 @Component({
   selector: 'google-maps',
@@ -20,14 +20,14 @@ export class GoogleMapsComponent implements OnInit{
 
     if (this.circuits.length == 0)
     {
-      this.markers.push(new MarkerItem("jajajja", this.lat, this.lng, "Test"));
+      this.markers.push(new Marker("jajajja", this.lat, this.lng, "Test"));
       console.log(this.markers.length);
     }
     else
     {
       for (let i = 0; i <this.circuits.length; i++)
       {
-        this.markers.push(new MarkerItem(this.circuits[i].circuitName, +this.circuits[i].Location.lat, +this.circuits[i].Location.long, this.circuits[i].Location.locality));
+        this.markers.push(new Marker(this.circuits[i].circuitName, +this.circuits[i].Location.lat, +this.circuits[i].Location.long, this.circuits[i].Location.locality));
       }
 
       console.log(this.markers.length);
