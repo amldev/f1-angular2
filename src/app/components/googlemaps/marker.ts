c35a9b75876b54018fa8ca134da80845a0ed6f72
Anartz Mugika <mugan86@gmail.com>
2017-03-18 10:02:57 +0100

    Define marker model adn interface

--- src/app/components/googlemaps/marker.ts
+++ src/app/components/googlemaps/marker.ts
@@ -1,3 +1,17 @@
+export class MarkerItem implements marker {
+  name: string;
+  lat:number;
+  lng:number;
+  locality:string;
+  constructor(name: string, lat: number, lng: number, locality: string)
+  {
+    this.name = name;
+    this.lat = lat;
+    this.lng = lng;
+    this.locality = locality;
+  }
+}
+
 export interface marker{
     name:string;
     lat:number;
