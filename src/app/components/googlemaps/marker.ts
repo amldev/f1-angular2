79f727c449606cad5da942361e674de13f1d32de
Anartz Mugika <mugan86@gmail.com>
2017-03-20 07:40:10 +0100

    Update Interface with documentatioN

--- src/app/components/googlemaps/marker.ts
+++ src/app/components/googlemaps/marker.ts
@@ -13,8 +13,20 @@ export class Marker implements MarkerIF {
 }
 
 export interface MarkerIF{
+    /**
+     * Marker name
+     */
     name:string;
+
+    /**
+     * Marker location.
+     * lat: Latitude / lng: Longitude
+     */
     lat:number;
     lng:number;
+
+    /**
+     * Locality to celebrate Grand Prix. For example: Barcelona (Spain)
+     */
     locality:string;
 }
