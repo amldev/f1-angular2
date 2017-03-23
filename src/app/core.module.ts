00918ee81ba261d0953ba134f8570794345cfd98
Anartz Mugika <mugan86@gmail.com>
2017-03-23 07:56:47 +0100

    Update imports references

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedService } from "./shared.service";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule
-    ]
+    ],
+     providers: [SharedService]
 })
 
 export class CoreModule {}
