73ecaaacdadcf1e7240f9c8256c8eb822de074a0
Anartz Mugika <mugan86@gmail.com>
2017-03-23 07:55:28 +0100

    Add app component inside components

---
+++ src/app/components/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  lat: number = 23.022505;  
+  lng: number = 72.571362;  
+}
