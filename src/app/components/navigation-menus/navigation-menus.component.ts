f9eadc81e519a21ceb723007ff7f4b0ffacf60b1
Anartz Mugika <mugan86@gmail.com>
2017-03-27 10:05:29 +0200

    Apply translations in navigation menu

--- src/app/components/navigation-menus/navigation-menus.component.ts
+++ src/app/components/navigation-menus/navigation-menus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from 'ng2-translate';
 
 @Component({
   selector: 'navigation-menus',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationMenusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate:TranslateService) {
+    console.info("Select language: " + localStorage.getItem('selectLanguage'));
+    if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
+    {
+      localStorage.setItem('selectLanguage', 'en');
+      console.info("Change language to " + localStorage.getItem('selectLanguage'));
+    }
+
+    translate.setDefaultLang(localStorage.getItem('selectLanguage')); 
+  }
 
   ngOnInit() {
   }
