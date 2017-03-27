b1fd8ee1d32d79f179c864c0e6ae103432867a12
Anartz Mugika <mugan86@gmail.com>
2017-03-27 09:21:12 +0200

    Load TranslateModule in Core Module

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,14 +1,22 @@
 import { NgModule  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedService } from "./shared.service";
+
+import { TranslateModule, TranslateLoader  } from 'ng2-translate/ng2-translate';
+import { createTranslateLoader } from './../services/translate-loader';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [Http]
+        })
     ],
      providers: [SharedService]
 })
