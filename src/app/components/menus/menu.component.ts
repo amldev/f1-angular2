1642d0e15cdbeaff203fd802f1954252ac99fa6f
Anartz Mugika <mugan86@gmail.com>
2017-03-27 09:21:31 +0200

    Start Apply translae

--- src/app/components/menus/menu.component.ts
+++ src/app/components/menus/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {TranslateService} from 'ng2-translate';
 
 @Component({
   selector: 'app-menu',
@@ -9,7 +10,16 @@ export class MenuComponent implements OnInit {
   @Input('title') title:string = "Formula 1 - Main";
   @Input('layoutType') layoutType: string = "";
 
-  constructor() { }
+  constructor (private translate:TranslateService) {
+    console.info("Select language: " + localStorage.getItem('selectLanguage'));
+    if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
+    {
+      localStorage.setItem('selectLanguage', 'en');
+      console.info("Change language to " + localStorage.getItem('selectLanguage'));
+    }
+
+    translate.setDefaultLang(localStorage.getItem('selectLanguage'));
+  }
 
   ngOnInit() {
 
