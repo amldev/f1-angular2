1e5295ad04da1a37b147c85c35ddcf7a11dff7ed
Anartz Mugika <mugan86@gmail.com>
2017-04-03 15:35:39 +0200

    Import angular animations and import material module correctly to use in app

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,7 +2,9 @@ import { NgModule  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedService } from "./shared.service";
+import { SharedService } from "./services/shared.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  { MaterialModule} from '@angular/material';
 
 import { TranslateModule, TranslateLoader  } from 'ng2-translate/ng2-translate';
 import { createTranslateLoader } from './services/translate-loader';
@@ -16,7 +18,8 @@ import { createTranslateLoader } from './services/translate-loader';
             provide: TranslateLoader,
             useFactory: (createTranslateLoader),
             deps: [Http]
-        })
+        }),
+        MaterialModule.forRoot(),
     ],
      providers: [SharedService]
 })
