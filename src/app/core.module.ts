fe2d976c8629cd0083f49c7bab481f4cb68d9436
Anartz Mugika <mugan86@gmail.com>
2017-04-07 14:42:24 +0200

    Return to Angular 2 Translate

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { SharedService } from "./services/shared.service";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import  { MaterialModule} from '@angular/material';
 
 import { TranslateModule, TranslateLoader  } from 'ng2-translate/ng2-translate';
