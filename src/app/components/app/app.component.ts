db9faa4e9d0a9a3c85fe971b7a89b9aae65a7433
Anartz Mugika <mugan86@gmail.com>
2017-04-10 08:51:49 +0200

    Initialize Keyboard events and start with define application navigation actions

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+window.focus(); // make sure we are on this page before we start typing
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +11,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   lat: number = 23.022505;  
   lng: number = 72.571362;  
+  constructor(public router: Router) {}
 
+  keyChange(event) {
+    console.log("Receive event" +  event[0] + " / " + event[1]);
+
+    if (event[1] == "39")
+    {
+      this.router.navigate(['/circuits']); 
+    }
+  }
   
 }
