24316df1a03609635cd7bf06fca23bb56d9e5949
Anartz Mugika <mugan86@gmail.com>
2017-04-16 18:20:59 +0200

    Create Circuits Urls contant

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -2,13 +2,17 @@ export const URL_LOCALHOST = "http://ergast.com/api/f1/";
 
 
 export const DRIVERS_URLS = 
-    {
-        ONE_TIME_WORLD_CHAMPION_F1 : "driverStandings/1/drivers.json?limit=100",
-    }
-;
+{
+    ONE_TIME_WORLD_CHAMPION_F1 : "driverStandings/1/drivers.json?limit=100",
+};
 
 export const SEASONS_URLS = 
 {
     ALL_SEASONS_URL : "seasons.json?limit=100"
-}
+};
+
+export const CIRCUITS_URLS =
+{
+
+};
 
