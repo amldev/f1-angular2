5cb5120ef3fd7ced8fcade5b807d30da2a7d0dc7
Anartz Mugika <mugan86@gmail.com>
2017-04-16 19:16:25 +0200

    Update with new constants

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -3,6 +3,8 @@ export const URL_LOCALHOST = "http://ergast.com/api/f1/";
 
 export const DRIVERS_URLS = 
 {
+    ALL_DRIVERS_SELECT: "/drivers.json",
+    SELECT_DRIVER_BEFORE_ID: "drivers/",
     ONE_TIME_WORLD_CHAMPION_F1 : "driverStandings/1/drivers.json?limit=100",
 };
 
