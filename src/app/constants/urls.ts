6905ad76c1c7893c2fb1c941867139b6a45d2be9
Anartz Mugika <mugan86@gmail.com>
2017-04-16 18:05:55 +0200

    Change urls constants and add new urls

--- src/app/constants/urls.ts
+++ src/app/constants/urls.ts
@@ -1,7 +1,14 @@
 export const URL_LOCALHOST = "http://ergast.com/api/f1/";
 
 
-export const DRIVERS_URLS = [
-    {ONE_TIME_WORLD_CHAMPION_F1 : "driverStandings/1/drivers.json?limit=100"}
-];
+export const DRIVERS_URLS = 
+    {
+        ONE_TIME_WORLD_CHAMPION_F1 : "driverStandings/1/drivers.json?limit=100",
+    }
+;
+
+export const SEASONS_URLS = 
+{
+    ALL_SEASONS_URL : "seasons.json?limit=100"
+}
 
