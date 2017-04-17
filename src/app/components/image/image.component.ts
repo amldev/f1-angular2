d07b87c6aa94e928f0ce6b5aad90eee1ad9f873e
Anartz Mugika <mugan86@gmail.com>
2017-04-17 08:30:56 +0200

    Test with image component

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -8,14 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ImageComponent implements OnInit {
   @Input('type') type:string = "";
   @Input('src') src:string;
-  default: string = "./assets/img/circuits/nocircuit.png";
+  default: string;
 
   constructor () {
     
   }
 
   ngOnInit() {
-
+    console.log(this.type);
+    if (this.type)
+    {
+        this.default = "./assets/img/flags/Unknown.png";
+        console.log("Default: " + this.default);
+    }
   }
 
 
