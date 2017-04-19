1149c7b89c815ffe58676d8e3198c5d611541327
Anartz Mugika <mugan86@gmail.com>
2017-04-19 18:09:11 +0200

    Create ResultModule to export Result component

---
+++ src/app/components/results/results.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { ResultsComponent } from './results.component';
+
+import { PipesModule } from '../../pipes';
+import { MenuModule} from './../menus/menu.module';
+
+import { TranslateModule, TranslateLoader  } from 'ng2-translate/ng2-translate';
+import { createTranslateLoader } from './../../services/translate-loader';
+import { Http } from '@angular/http';
+
+@NgModule({
+  imports: [
+    MenuModule,
+    PipesModule,
+    CommonModule,
+    RouterModule,
+    TranslateModule.forRoot({
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [Http]
+        })
+  ],
+  declarations: [ResultsComponent],
+  exports: [ResultsComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class ResultsModule { }
