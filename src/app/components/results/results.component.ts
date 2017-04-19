66e1802b93e4f516d74456e5cb6a9aec4777fadb
Anartz Mugika <mugan86@gmail.com>
2017-04-19 18:36:54 +0200

    Log receive input value in ngOnInit

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -9,7 +9,7 @@ import {TranslateService} from 'ng2-translate';
 export class ResultsComponent implements OnInit {
     @Input('type') type: String;
     constructor(private translate:TranslateService) {
-        console.log(this.type);
+        
         console.info("Select language: " + localStorage.getItem('selectLanguage'));
         if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
         {
@@ -21,6 +21,7 @@ export class ResultsComponent implements OnInit {
     }
 
     ngOnInit() {
+        console.log(this.type);
     }
 
 }
