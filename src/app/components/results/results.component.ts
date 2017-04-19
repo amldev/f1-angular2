135c699b36fc5e203224a076bf71877fc9550160
Anartz Mugika <mugan86@gmail.com>
2017-04-19 18:40:37 +0200

    Import Drive Services to get data about last race and current ranking in F1

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {TranslateService} from 'ng2-translate';
+import {DriversService} from './../../services/drivers.service';
 
 @Component({
   selector: 'results',
@@ -8,7 +9,7 @@ import {TranslateService} from 'ng2-translate';
 })
 export class ResultsComponent implements OnInit {
     @Input('type') type: String;
-    constructor(private translate:TranslateService) {
+    constructor(private translate:TranslateService, private _sharedService: DriversService) {
         
         console.info("Select language: " + localStorage.getItem('selectLanguage'));
         if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
