98695008808002167bcb11c7561c246a6fb14af6
Anartz Mugika <mugan86@gmail.com>
2017-04-19 18:37:10 +0200

    Remove unused value

--- src/app/components/navigation-menus/navigation-menus.component.ts
+++ src/app/components/navigation-menus/navigation-menus.component.ts
@@ -7,7 +7,6 @@ import {TranslateService} from 'ng2-translate';
   styleUrls: ['./navigation-menus.component.css']
 })
 export class NavigationMenusComponent implements OnInit {
-
   constructor(private translate:TranslateService) {
     console.info("Select language: " + localStorage.getItem('selectLanguage'));
     if (localStorage.getItem('selectLanguage') == null || localStorage.getItem('selectLanguage') == "null")
