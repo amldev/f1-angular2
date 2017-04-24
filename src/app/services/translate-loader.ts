758b56d3634e0822330f34edb155e327509a9264
Anartz Mugika <mugan86@gmail.com>
2017-04-24 08:12:43 +0200

    Change to new version of use in Angular 4

--- src/app/services/translate-loader.ts
+++ src/app/services/translate-loader.ts
@@ -1,6 +1,6 @@
-import { TranslateStaticLoader } from 'ng2-translate/ng2-translate';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { Http } from '@angular/http';
 
 export function createTranslateLoader(http: Http) {
-    return new TranslateStaticLoader(http, 'assets/i18n', '.json');
+    return new TranslateHttpLoader(http, 'https://raw.githubusercontent.com/mugan86/angular-4-playground/master/src/assets/i18n/', '.json');
 }
